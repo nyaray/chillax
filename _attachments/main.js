6c2e7110d1e87c412790c945fafa2bd16770ef61
Emilio Nyaray <emilio@nyaray.com>
2011-02-11 18:10:20 +0100

    Tasks are now CRUDable, bar moving them between projects. Also, lots of small changes..

--- _attachments/main.js
+++ _attachments/main.js
@@ -1,6 +1,34 @@
 var $db = $.couch.db("chillax");
 var currentProject = "inbox";
 
+function writeTask(task, callback) {
+  $db.saveDoc(task, {"success":callback});
+}
+
+function updateTask(task, callback) {
+  // do smart stuff
+  $db.openDoc(task.id, {"success": function(oldTask) {
+    var storeTask = {};
+    storeTask._id     = oldTask._id;
+    storeTask._rev    = oldTask._rev;
+    storeTask.type    = "task";
+    storeTask.name    = task.name;
+    storeTask.project = task.project;
+
+    if (task.due && typeof(task.due) == "string" && task.due != "")
+      storeTask = task.due;
+
+    if (task.desc && typeof(task.desc) == "string" && task.desc != "")
+      storeTask.desc = task.desc;
+
+    if (task.complete && task.complete == true)
+      storeTask.complete = task.complete;
+
+    //console.log(storeTask);
+    writeTask(storeTask, callback);
+  }});
+}
+
 function refreshProjectList() {
   var projectListRefresher = function(data) {
     for (i in data.rows) {
@@ -25,64 +53,98 @@ function refreshProjectList() {
     {"success": projectListRefresher});
 }
 
+function generateTasklistItems(taskContainer, rows) {
+  var tasklistItems = "";
+
+  for (i in rows) {
+    var payload  = rows[i].value;
+
+    var id       = rows[i].id;
+    var name     = payload.name;
+    var due      = (payload.due)? payload.due: "";
+    var desc     = (payload.desc)? payload.desc: "";
+    var complete = (payload.complete && payload.complete == true)?
+      payload.complete: false;
+    
+    var completeStr = ((complete)? "checked ": "");
+    var html =
+      '<li class="task" id="'+ id +'">'+
+      '  <input class="taskcompleted" type="checkbox" '+
+      '    value="complete" '+ completeStr +'/>'+
+      '  <span class="taskname">'+ name +'</span>'+
+      '  <span class="taskend">'+
+        ((due != "")? '<span class="taskdue">'+due+'</span>': "")+
+      '    <span class="taskdelete">x</span>'+
+      '  </span>'+
+        ((desc != "")? '<span class="taskdesc">'+desc+'</span>': "")+
+      '</li>'+
+      '<li class="taskeditor">'+
+      '  <form>'+
+      '    <h4>Edit task</h4>'+
+      '    <input class="editcomplete" type="checkbox" '+
+      '      value="complete" '+ completeStr +'/>'+
+      '    <input placeholder="Name" class="editname" '+
+      '      value="'+ name +'" type="text" />'+
+      '    <input placeholder="Due" class="editdue" '+
+      '      value="'+ due +'" type="text" /><br>'+
+      '    <textarea placeholder="Description" class="editdesc"'+
+      '      name="editdesc" type="text">'+ desc +'</textarea><br> '+
+      '    <button type="button" class="editsubmit">Save</button>'+
+      '    <button type="button" class="editcancel">Cancel</button>'+
+      '  </form>'+
+      '</li>';
+
+    tasklistItems += html;
+  }
+
+  return tasklistItems;
+}
+
 function refreshTaskList() {
   $('#tasklist').empty();
 
   $db.view('chillax/projecttasks', {
     "key":currentProject,
+    "error":function() {
+      $("#tasklist").html("<li>An error occurred :(</li>");
+    },
     "success":function(data) {
-      var taskContainer = $('#tasklist');
       dbg.prepend("loaded "+data.rows.length+" tasks, viewing project:"+
         currentProject+"<br />");
+      var taskContainer = $('#tasklist');
 
       if(data.rows.length > 0)
       {
-        var rows = data.rows;
-        for (i in rows) {
-          var payload  = rows[i].value;
+        // setup content
+        $("#tasklist").html(generateTasklistItems(taskContainer, data.rows));
+        $(".taskeditor").hide();
 
-          var id   = rows[i].id;
-          var name = payload.name;
-          var due  = (payload.due)?
-            '<span class="taskdue">'+ payload.due +'</span>': "";
-          var desc = (payload.desc)?
-            '<div class="taskdesc">'+ payload.desc +'</div>': "";
-
-          var html =
-            '<li class="task" id="'+ id +'">'+
-            '  <span class="taskname">'+ name +'</span>'+
-            '  <span class="taskend">'+
-                due+
-            '    <span class="taskdelete">x</span>'+
-            '  </span>'+
-              desc+
-            '</li>'+
-            '<li class="taskeditor">'+
-            '  <form>'+
-            '    <legend>Edit task</legend>'+
-            '    <input placeholder="Name" class="editname" name="editname"'+
-            '      value="'+ name +'" type="text">'+
-            '    <input placeholder="Due" class="editdue" name="editdue"'+
-            '      value="'+ due +'" type="text"><br>'+
-            '    <textarea placeholder="Description" class="editdesc"'+
-            '      name="editdesc" type="text">'+ desc +'</textarea><br> '+
-            '    <button type="button" class="editsubmit">Save</button>'+
-            '    <button type="button" class="editcancel">Cancel</button>'+
-            '  </form>'+
-            '</li>';
-          $(taskContainer).append(html);
-        }
-
-        $(".taskeditor").animate({"height":"toggle"}, {"duration":0});
-
+        // set actions
         $(".task").dblclick(function() {
-          $(this).next().slideDown("fast");
-          $(this).slideUp("fast");
+          $(this).siblings(".taskeditor").hide();
+          $(this).next().show();
+          $(this).hide();
+        });
+
+        $('.task input[type="checkbox"]').click(function() {
+          var editor = $($(this).parents(".task")).next();
+          editor.find('input[type="checkbox"]').click();
+          editor.find(".editsubmit").click();
+
+          return false;
         });
 
         $(".taskeditor").dblclick(function() {
-          $(this).prev().slideDown("fast");
-          $(this).slideUp("fast");
+          $(this).find(".editcancel").click();
+          $(this).siblings(".taskeditor").hide();
+          $(this).prev().show();
+          $(this).hide();
+        });
+
+        $('.taskeditor input, .taskeditor button, .taskeditor textarea')
+        .dblclick(function(event) {
+          event.preventDefault();
+          return false;
         });
 
         $(".taskdelete").click(function(event) {
@@ -91,17 +153,42 @@ function refreshTaskList() {
           var task   = target.parents(".task");
           var taskID = task.attr('id');
           $db.openDoc(taskID, {"success":function(doc) {
-            dbg.prepend("delete document opened<br />");
-            var ret = $db.removeDoc(doc, {"success":function() {
+            dbg.prepend("delete document opened, attempting delete<br />");
+            $db.removeDoc(doc, {"success":function() {
               dbg.prepend("delete confirmed<br />");
+              task.hide();
               task.remove();
             }});
-            dbg.prepend("ret:"+ret+"<br />");
           }});
 
           return false;
         });
 
+        $(".editsubmit").click(function() {
+          var item     = $(this).parents(".taskeditor");
+          var id       = item.prev().attr("id");
+          var name     = item.find(".editname").val();
+          var due      = item.find(".editdue").val();
+          var desc     = item.find(".editdesc").val();
+          var complete = item.find(".editcomplete").attr("checked");
+
+          var task     = {};
+          task.type    = "task";
+          task.project = currentProject;
+          task.id      = id;
+          task.name    = name;
+
+          if (due      != "") task.due        = due;
+          if (desc     != "") task.desc       = desc;
+          if (complete == true) task.complete = true;
+
+          updateTask(task, function() {
+            dbg.prepend("Saved changes<br />");
+            refreshTaskList();
+          });
+
+          return false;
+        });
       }
   }});
 }
@@ -127,21 +214,24 @@ function createProject(project, callback) {
   // do stuff to store project in couch
 }
 
-function writeTask(task, callback) {
-  // do stuff to store task in couch
-  $db.saveDoc(task, {"success":callback});
-}
-
 $(document).ready(function() {
   dbg = $("#debug");
 
   // load data from couch
   refreshProjectList();
+
+  // TODO: Check if the hash in the url is set, load the corresponding project
+  // instead, if it is.
+  // set view to inbox
   changeProject("inbox", "Inbox");
 
   $("form .cancel").click(function() {
     $(this).parents("form").find("input, textarea").val('');
     return false;
+  });
+
+  $('form input[type="text"]').keyup(function(e) {
+    e.preventDefault();
   });
 
   $("form .addsubmit").click(function() {
--- views/projecttasks/map.js
+++ views/projecttasks/map.js
@@ -2,14 +2,19 @@ function(doc) {
   if(doc.type && doc.type == "task")
   {
     // bare minimum for an emitted task
-    name = (doc.name)? doc.name: "";
-    task = {"name":name};
+    project = (doc.project)? doc.project: "";
+    name    = (doc.name)? doc.name: "";
+
+    task         = {};
+    task.type    = doc.type;
+    task.project = project;
+    task.name    = name;
 
     // avoid adding properties that we don't use
     if(doc.desc && doc.desc != "") task.desc = doc.desc;
     if(doc.due && doc.due != "") task.due = doc.due;
+    if(doc.complete && doc.complete == true) task.complete = doc.complete;
 
-    project = (doc.project)? doc.project: "";
     emit(project, task);
   }
 }
